29aef72fec05c62ba8bdd94fc2ee105944dc6c5e
a18608407391 <https://github.com/a18608407391/MvvM.git>
2020-09-06 21:43:50 +0800

    修改部分代码

--- app/src/main/java/com/buyer/redman/fragment/MainFragment.kt
+++ app/src/main/java/com/buyer/redman/fragment/MainFragment.kt
@@ -53,6 +53,7 @@ class MainFragment : BaseFragment<FragmentMainBinding, MainFragmentViewModel>() 
 //                R.id.menu_system -> mainViewpager.setCurrentItem(2, false)
 //                R.id.menu_public -> mainViewpager.setCurrentItem(3, false)
 //                R.id.menu_me -> mainViewpager.setCurrentItem(4, false)
+
                 }
             }
         }
